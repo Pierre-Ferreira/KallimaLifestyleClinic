5b344f42360b7718bf193ae88b1d3f62fd0dd529
Pierre Ferreira <pierre@tektite.biz>
2018-07-04 22:10:35 +0200

    Client Info Comp #2.

--- imports/ui/components/TabsComponents/ClientPersonalInfoComp.jsx
+++ imports/ui/components/TabsComponents/ClientPersonalInfoComp.jsx
@@ -1,7 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Alert, Button, ButtonToolbar, OverlayTrigger, Popover, Form, FormGroup, Col, FormControl } from 'react-bootstrap';
+import { Alert, Button, ButtonToolbar, OverlayTrigger, Popover, Form, FormGroup, Col, FormControl, ControlLabel } from 'react-bootstrap';
 import moment from 'moment/moment'
 import './ClientPersonalInfoComp.less';
 
@@ -11,11 +11,21 @@ export default class ClientPersonalInfoComp extends Component {
     this.state = {
     };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleSurnameChange = this.handleSurnameChange.bind(this);
   }
 
   handleNameChange(e) {
+    console.log('handleNameChange:', e.target.value)
     this.setState({
       name: e.target.value,
+    });
+  }
+
+  handleSurnameChange(e) {
+    console.log('handleSurnameChange:', e.target.value)
+    this.setState({
+      surname: e.target.value,
     });
   }
 
@@ -65,12 +75,153 @@ export default class ClientPersonalInfoComp extends Component {
         </div>
         <Form horizontal>
           <div className="middle-tier-area">
-            <FormGroup controlId="formHorizontalName">
-              <Col sm={2}>
-                Name
+            <FormGroup controlId="name-formgroup">
+              <Col mdOffset={1} md={3}>
+                <ControlLabel>Name:</ControlLabel>
               </Col>
-              <Col sm={10}>
-                <FormControl type="text" placeholder="Name" />
+              <Col md={7}>
+                <FormControl
+                  className=""
+                  type="text"
+                  placeholder="Name"
+                  value={this.state.name}
+                  onChange={this.handleNameChange}
+                />
+              </Col>
+            </FormGroup>
+            <FormGroup
+              controlId="surname-formgroup"
+            >
+              <Col mdOffset={1} md={3}>
+                <ControlLabel>Surname:</ControlLabel>
+              </Col>
+              <Col md={7}>
+                <FormControl
+                  type="text"
+                  placeholder="Surname"
+                  value={this.state.surname}
+                  onChange={this.handleSurnameChange}
+                />
+              </Col>
+            </FormGroup>
+            <FormGroup
+              controlId="cellno-formgroup"
+            >
+              <Col mdOffset={1} md={3}>
+                <ControlLabel>Cell No:</ControlLabel>
+              </Col>
+              <Col md={7}>
+                <FormControl
+                  type="text"
+                  placeholder="Cell No"
+                  value={this.state.cellno}
+                  onChange={this.handleCellnoChange}
+                />
+              </Col>
+            </FormGroup>
+            <FormGroup
+              controlId="workno-formgroup"
+            >
+              <Col mdOffset={1} md={3}>
+                <ControlLabel>Work No:</ControlLabel>
+              </Col>
+              <Col md={7}>
+                <FormControl
+                  type="text"
+                  placeholder="Work No"
+                  value={this.state.workno}
+                  onChange={this.handleWorknoChange}
+                />
+              </Col>
+            </FormGroup>
+            <FormGroup
+              controlId="email-formgroup"
+            >
+              <Col mdOffset={1} md={3}>
+                <ControlLabel>Email:</ControlLabel>
+              </Col>
+              <Col md={7}>
+                <FormControl
+                  type="text"
+                  placeholder="Email"
+                  value={this.state.email}
+                  onChange={this.handleEmailChange}
+                />
+              </Col>
+            </FormGroup>
+            <FormGroup
+              controlId="married-formgroup"
+            >
+              <Col mdOffset={1} md={3}>
+                <ControlLabel>Married:</ControlLabel>
+              </Col>
+              <Col md={7}>
+                <FormControl
+                  type="text"
+                  placeholder="Married"
+                  value={this.state.married}
+                  onChange={this.handleMarriedChange}
+                />
+              </Col>
+            </FormGroup>
+            <FormGroup
+              controlId="children-formgroup"
+            >
+              <Col mdOffset={1} md={3}>
+                <ControlLabel>Children:</ControlLabel>
+              </Col>
+              <Col md={7}>
+                <FormControl
+                  type="text"
+                  placeholder="Children"
+                  value={this.state.children}
+                  onChange={this.handleChildrenChange}
+                />
+              </Col>
+            </FormGroup>
+            <FormGroup
+              controlId="hobbies-formgroup"
+            >
+              <Col mdOffset={1} md={3}>
+                <ControlLabel>Hobbies:</ControlLabel>
+              </Col>
+              <Col md={7}>
+                <FormControl
+                  componentClass="textarea"
+                  placeholder="Hobbies"
+                  value={this.state.hobbies}
+                  onChange={this.handleHobbiesChange}
+                />
+              </Col>
+            </FormGroup>
+            <FormGroup
+              controlId="occupation-formgroup"
+            >
+              <Col mdOffset={1} md={3}>
+                <ControlLabel>Occupation:</ControlLabel>
+              </Col>
+              <Col md={7}>
+                <FormControl
+                  type="text"
+                  placeholder="Occupation"
+                  value={this.state.occupation}
+                  onChange={this.handleOccupationChange}
+                />
+              </Col>
+            </FormGroup>
+            <FormGroup
+              controlId="otherinfo-formgroup"
+            >
+              <Col mdOffset={1} md={3}>
+                <ControlLabel>Other Info:</ControlLabel>
+              </Col>
+              <Col md={7}>
+                <FormControl
+                  componentClass="textarea"
+                  placeholder="Other info"
+                  value={this.state.otherinfo}
+                  onChange={this.handleOtherInfoChange}
+                />
               </Col>
             </FormGroup>
           </div>

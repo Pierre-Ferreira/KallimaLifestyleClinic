2b6f6940732789d21a47fbd281a872d47d3f022c
Pierre Ferreira <pierre@tektite.biz>
2018-08-06 16:45:57 +0200

    Uploading to S3 #5.

---
+++ server/policy.js
@@ -0,0 +1,16 @@
+
+// BrowserPolicy.content.allowOriginForAll('blob:');
+// console.log('BROWSER POLICY BEFORE:',BrowserPolicy)
+// //*************
+// BrowserPolicy.framing.disallow();
+// BrowserPolicy.content.disallowInlineScripts();
+// BrowserPolicy.content.disallowEval();
+// // BrowserPolicy.content.allowInlineStyles();
+BrowserPolicy.content.allowOriginForAll('fonts.googleapis.com');
+BrowserPolicy.content.allowOriginForAll('fonts.gstatic.com');
+// BrowserPolicy.content.allowOriginForAll('kallima-pictures.amazonaws.com');
+// //*************
+// BrowserPolicy.content.allowImageOrigin("blob:");
+// var constructedCsp = BrowserPolicy.content._constructCsp();
+// BrowserPolicy.content.setPolicy(constructedCsp +" media-src blob:;");
+console.log('BROWSER POLICY SET')

91ec79b6e436d5b95b2cba8708843a2dceda3702
Pierre Ferreira <pierre@tektite.biz>
2018-08-07 19:26:24 +0200

    Removed browser-policy.

--- server/policy.js
+++ server/policy.js
@@ -6,8 +6,10 @@
 // BrowserPolicy.content.disallowInlineScripts();
 // BrowserPolicy.content.disallowEval();
 // // BrowserPolicy.content.allowInlineStyles();
-BrowserPolicy.content.allowOriginForAll('fonts.googleapis.com');
-BrowserPolicy.content.allowOriginForAll('fonts.gstatic.com');
+// BrowserPolicy.content.allowOriginForAll('fonts.googleapis.com');
+// BrowserPolicy.content.allowOriginForAll('fonts.gstatic.com');
+// BrowserPolicy.content.allowOriginForAll('http://*.amazonaws.com');
+// BrowserPolicy.content.allowOriginForAll('https://*.amazonaws.com');
 // BrowserPolicy.content.allowOriginForAll('kallima-pictures.amazonaws.com');
 // //*************
 // BrowserPolicy.content.allowImageOrigin("blob:");

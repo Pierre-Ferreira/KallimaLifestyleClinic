717bd35452978e7c6a84556daaf78c9d5e14f18e
Pierre <pierre@Pierres-MacBook-Pro.local>
2021-04-22 22:26:30 +0200

    Fixing email issues.

--- server/accounts/emails.js
+++ server/accounts/emails.js
@@ -1,6 +1,6 @@
 import { Accounts } from 'meteor/accounts-base';
 Accounts.emailTemplates.siteName = 'Kallima Lifestyle Clinic';
-Accounts.emailTemplates.from = 'Kallima Lifestyle Clinic <admin@kallima-office.co.za>';
+Accounts.emailTemplates.from = 'Kallima Lifestyle Clinic <therapyroom24@gmail.com>';
 
 // Email template for verification emails.
 Accounts.emailTemplates.verifyEmail = {
@@ -10,7 +10,7 @@ Accounts.emailTemplates.verifyEmail = {
   text(user, url) {
     // Do not remove the # out of url or change verification url!
     const emailAddress = user.emails[0].address;
-    const supportEmail = 'admin@kallima-office.co.za';
+    const supportEmail = 'therapyroom24@gmail.com';
     const emailBody = `To verify your email address (${emailAddress}) visit the following link:\n\n${url}\n\n If you did not request this verification, please ignore this email. If you feel something is wrong, please contact our support team: ${supportEmail}.`;
 
     return emailBody;
@@ -26,7 +26,7 @@ Accounts.emailTemplates.resetPassword = {
     const emailAddress = user.emails[0].address;
     const urlWithoutHash = url.replace('#/', '');
     const urlCorrect = urlWithoutHash.replace('/reset-password', '/auth/reset-password');
-    const supportEmail = 'admin@kallima-office.co.za';
+    const supportEmail = 'therapyroom24@gmail.com';
     const emailBody = `To reset the password for (${emailAddress}) visit the following link:\n\n${urlCorrect}\n\n If you did not request this verification, please ignore this email. If you feel something is wrong, please contact our support team: ${supportEmail}.`;
     return emailBody;
   },
